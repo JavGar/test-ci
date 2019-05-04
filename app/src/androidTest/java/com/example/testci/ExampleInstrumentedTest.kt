eb4c38bf470491d1daaa771d68df7ebc4b75e724
JavGar <dushtajavier@hotmail.com>
2019-05-04 00:17:20 -0500

    Update test fail

--- app/src/androidTest/java/com/example/testci/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/testci/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.example.testci", appContext.packageName)
+        assertEquals("com.example.testcic", appContext.packageName)
     }
 }
